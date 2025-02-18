c75ae398deb26312fcce848215cfb364194007bc
Aayush <ayushkarkee6@gmail.com>
2025-02-18 17:06:51 +0545

    update :: added service data in data.ts

---
+++ src/lib/data.tsx
@@ -0,0 +1,20 @@
+import { Car, CreditCard, MapPin, Wrench } from "lucide-react";
+
+const services = [
+  {
+    name: "Car Parts from Trusted Vendors",
+    icon: <Wrench className="w-10 h-10 text-indigo-700" />,
+  },
+  {
+    name: "Mechanic Services",
+    icon: <Car className="w-10 h-10 text-indigo-700" />,
+  },
+  {
+    name: "Emergency Assistance & Live Tracking",
+    icon: <MapPin className="w-10 h-10 text-indigo-700" />,
+  },
+  {
+    name: "Secure Payments",
+    icon: <CreditCard className="w-10 h-10 text-indigo-700" />,
+  },
+];
