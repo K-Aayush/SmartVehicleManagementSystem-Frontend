501c4a86842778fb74a126d10d0787c210cdc664
Aayush <ayushkarkee6@gmail.com>
2025-02-18 15:28:03 +0545

    update :: creating folders for landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
+import About from "../components/About";
+import Banner from "../components/Banner";
+import Hero from "../components/Hero";
+import Questions from "../components/Questions";
+import Service from "../components/Service";
+import Testimonials from "../components/Testimonials";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container mx-auto px-4 py-16">
+      <Hero />
+      <About />
+      <Service />
+      <Banner />
+      <div className="mb-24">
+        <h2 className="text-4xl font-semibold mb-12 gradient-title">
+          What our users say
+        </h2>
+        <Testimonials />
+      </div>
+      <Questions />
+    </div>
+  );
 };
 
 export default Home;
