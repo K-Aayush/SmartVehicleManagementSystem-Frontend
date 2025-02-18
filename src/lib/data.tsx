57f4b3dd56cc6d81fb7f07302fcbae50c42a7dd7
Aayush <ayushkarkee6@gmail.com>
2025-02-18 17:11:01 +0545

    update :: added banner data in data.trsx

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -1,4 +1,4 @@
-import { Car, CreditCard, MapPin, Wrench } from "lucide-react";
+import { Car, Check, CreditCard, MapPin, Users, Wrench } from "lucide-react";
 
 export const services = [
   {
@@ -16,5 +16,23 @@ export const services = [
   {
     name: "Secure Payments",
     icon: <CreditCard className="w-10 h-10 text-indigo-700" />,
+  },
+];
+
+export const banner = [
+  {
+    name: "Active Users",
+    icon: <Users className="w-10 h-10 text-indigo-700" />,
+    number: "2M+",
+  },
+  {
+    name: "Services Delivered",
+    icon: <Check className="w-10 h-10 text-indigo-700" />,
+    number: "2,200+",
+  },
+  {
+    name: "Trusted Vendors & Mechanics",
+    icon: <Users className="w-10 h-10 text-indigo-700" />,
+    number: "100+",
   },
 ];
