1744adea6c17bcf43ef9fbee48546d930feaf2b3
Aayush <ayushkarkee6@gmail.com>
2025-02-21 01:15:23 +0545

    update :: making redirected if authenticated middleware

--- src/middleware/RedirectIfAuthenticated.tsx
+++ src/middleware/RedirectIfAuthenticated.tsx
@@ -1,8 +1,44 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { jwtDecode } from "jwt-decode";
+import { Navigate } from "react-router-dom";
 
-const RedirectIfAuthenticated = () => {
-  return (
-    <div>RedirectIfAuthenticated</div>
-  )
+interface DecodedToken {
+  id: string | null;
+  role: string | null;
 }
 
-export default RedirectIfAuthenticated
+const RedirectIfAuthenticated = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { token, isLoading } = useContext(AppContext);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!token) {
+    return <Navigate to={"/"} replace />;
+  }
+
+  const decoded = jwtDecode<DecodedToken>(token);
+
+  if (decoded) {
+    switch (decoded.role) {
+      case "USER":
+        return <Navigate to={"/user/dashboard"} replace />;
+      case "VENDOR":
+        return <Navigate to={"/vendor/dashboard"} replace />;
+      case "SERVICE_PROVIDER":
+        return <Navigate to={"/service-provider/dashboard"} replace />;
+      case "ADMIN":
+        return <Navigate to={"/admin/dashboard"} replace />;
+      default:
+        return <Navigate to={"/"} replace />;
+    }
+  }
+
+  return children;
+};
+
+export default RedirectIfAuthenticated;
