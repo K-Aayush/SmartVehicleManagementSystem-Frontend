c264d415de835eabe503bdafd361e46cb0819624
Aayush <ayushkarkee6@gmail.com>
2025-03-08 22:28:34 +0545

    creating user table component

---
+++ src/components/admin/ViewUserTable.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewUserTable = () => {
+  return <div>ViewUserTable</div>;
+};
+
+export default ViewUserTable;
--- src/pages/admin/ManageUsers.tsx
+++ src/pages/admin/ManageUsers.tsx
@@ -1,16 +1,18 @@
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
-
+import ViewUserTable from "../../components/admin/ViewUserTable";
 
 const ManageUsers = () => {
-  const { error, isLoading } =
-    useContext(AppContext);
+  const { error, isLoading } = useContext(AppContext);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
 
-  
-  return <div>ManageUsers</div>;
+  return (
+    <div>
+      <ViewUserTable />
+    </div>
+  );
 };
 
 export default ManageUsers;
