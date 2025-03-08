e6dad89ff030e300808daf772366a8b5cb9793ea
Aayush <ayushkarkee6@gmail.com>
2025-03-08 22:27:27 +0545

    added table component from sadcn ui

--- src/pages/admin/ManageUsers.tsx
+++ src/pages/admin/ManageUsers.tsx
@@ -1,17 +1,15 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 
-import { AllUsersState } from "../../lib/types";
-import axios, { AxiosError } from "axios";
 
 const ManageUsers = () => {
-  const { error, isLoading, setError, setIsLoading, backendUrl, token } =
+  const { error, isLoading } =
     useContext(AppContext);
-
-
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
+
+  
   return <div>ManageUsers</div>;
 };
 
