6b0d37799579111db7f11e971d7c31aea0f52016
Aayush <ayushkarkee6@gmail.com>
2025-03-08 23:25:27 +0545

    update :: make the user view based on dropdown selected

--- src/pages/admin/ManageUsers.tsx
+++ src/pages/admin/ManageUsers.tsx
@@ -1,21 +1,51 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import ViewUserTable from "../../components/admin/ViewUserTable";
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../../components/ui/select";
+
 const ManageUsers = () => {
   const { error, isLoading, allUsers } = useContext(AppContext);
+  const [selectedRole, setSelectedRole] =
+    useState<keyof typeof allUsers>("TOTAL");
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
 
   return (
     <div className="p-4 mx-4">
-      {Object.entries(allUsers).map(([role, users]) => (
-        <div key={role}>
-          <h2 className="my-4 text-lg font-bold">{role} Users</h2>
-          <ViewUserTable users={users} />
-        </div>
-      ))}
+      <Select
+        defaultValue={selectedRole}
+        onValueChange={(value) =>
+          setSelectedRole(value as keyof typeof allUsers)
+        }
+      >
+        <SelectTrigger className="w-[200px]">
+          <SelectValue>
+            {selectedRole === "TOTAL" ? "All Users" : selectedRole}
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="TOTAL">All Users</SelectItem>
+            <SelectItem value="USER">Customers</SelectItem>
+            <SelectItem value="VENDOR">Vendors</SelectItem>
+            <SelectItem value="SERVICE_PROVIDER">Mechanics</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+      <div className="mt-4">
+        <ViewUserTable users={allUsers[selectedRole]} />
+      </div>
     </div>
   );
 };
