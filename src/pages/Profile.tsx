521ef60e7499587132b76d78d8d2e9fc30439742
Aayush <ayushkarkee6@gmail.com>
2025-03-09 14:54:26 +0545

    styled profil page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,6 +5,7 @@ import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Button } from "../components/ui/button";
 
+
 const Profile = () => {
   const { userData, isLoading } = useContext(AppContext);
 
