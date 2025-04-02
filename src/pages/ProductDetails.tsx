9926ee5f62a79d54ed159ed4a7e76f573272df11
Aayush <ayushkarkee6@gmail.com>
2025-04-02 20:55:29 +0545

    fetched the product by id

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -2,6 +2,7 @@ import { useCallback, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { Product } from "../lib/types";
+import axios from "axios";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -11,9 +12,16 @@ const ProductDetails = () => {
     window.scrollTo(0, 0);
   }, [id]);
 
-  const { backendUrl, setIsLoading, isLoading, products, setProducts } =
-    useContext(AppContext);
+  const {
+    backendUrl,
+    setIsLoading,
+    isLoading,
+    products,
+    setProducts,
+    setError,
+  } = useContext(AppContext);
   const [productByCategory, setProductByCategory] = useState<Product[]>([]);
+  const [productData, setProductData] = useState<Product | null>(null);
 
   //function to fetch product by category
   const fetchProductByCategory = useCallback(
@@ -28,7 +36,30 @@ const ProductDetails = () => {
     [products]
   );
 
-  
+  // fetching the product data by id
+  useEffect(() => {
+    const fetchProductData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(
+          `${backendUrl}/api/vendor/getProductById/${id}`
+        );
+
+        if (data.success) {
+          setProductData(data.product);
+          console.log(data.product);
+        } else {
+          setError(data.error);
+        }
+      } catch (error) {
+        console.log("product fetching error:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchProductData();
+  }, [id, backendUrl, setError, setIsLoading]);
+
   return <div>ProductDetails</div>;
 };
 
