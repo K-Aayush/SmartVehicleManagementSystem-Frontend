8c4a11156e462cd6e1040cd1667682d0f1a0b7b9
Aayush <ayushkarkee6@gmail.com>
2025-04-02 20:43:39 +0545

    update :: created the function to fetch product by categiry

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,4 +1,34 @@
+import { useCallback, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { Product } from "../lib/types";
+
 const ProductDetails = () => {
+  const { id } = useParams();
+
+  //scroll to top when route changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [id]);
+
+  const { backendUrl, setIsLoading, isLoading, products, setProducts } =
+    useContext(AppContext);
+  const [productByCategory, setProductByCategory] = useState<Product[]>([]);
+
+  //function to fetch product by category
+  const fetchProductByCategory = useCallback(
+    (categoryName: string) => {
+      if (products.length === 0) return;
+      const filteredProductList = products.filter(
+        (product) =>
+          product.category.toLowerCase() === categoryName.toLowerCase()
+      );
+      setProductByCategory(filteredProductList);
+    },
+    [products]
+  );
+
+  
   return <div>ProductDetails</div>;
 };
 
