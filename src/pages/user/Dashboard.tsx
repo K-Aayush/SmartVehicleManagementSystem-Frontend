e33171ceaebf260d4b7f312c47af2aad371099e6
Aayush <ayushkarkee6@gmail.com>
2025-04-25 18:06:13 +0545

    chat

--- src/pages/user/Dashboard.tsx
+++ src/pages/user/Dashboard.tsx
@@ -1,11 +1,16 @@
-import { LayoutDashboard, Settings, ShoppingBag } from "lucide-react";
+import {
+  LayoutDashboard,
+  Settings,
+  ShoppingBag,
+  MessageSquare,
+  AlertTriangle,
+} from "lucide-react";
 import { NavLink, Outlet } from "react-router-dom";
 
 const UserDashboard = () => {
   return (
     <div className="min-h-screen">
       <div className="flex items-start overflow-y-auto">
-        {/*sidebar for user panel*/}
         <div className="inline-block min-h-screen border-r-2">
           <ul className="flex flex-col items-start pt-5 text-gray-800 dark:text-gray-300">
             <NavLink
@@ -31,7 +36,28 @@ const UserDashboard = () => {
               <ShoppingBag className="w-5 h-5" />
               <p>My Orders</p>
             </NavLink>
-
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-700 ${
+                  isActive && "bg-gray-700 border-r-4 border-primary"
+                }`
+              }
+              to={"/user/dashboard/chat"}
+            >
+              <MessageSquare className="w-5 h-5" />
+              <p>Messages</p>
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-700 ${
+                  isActive && "bg-gray-700 border-r-4 border-primary"
+                }`
+              }
+              to={"/user/dashboard/emergency"}
+            >
+              <AlertTriangle className="w-5 h-5" />
+              <p>Emergency Service</p>
+            </NavLink>
             <NavLink
               className={({ isActive }) =>
                 `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-700 ${
