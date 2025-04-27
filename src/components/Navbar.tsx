4e6b6a7f3142ca406b0b592c7b988ea38a156ffa
Aayush <ayushkarkee6@gmail.com>
2025-04-28 00:40:50 +0545

    update :: removed cart feature for other user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,26 +4,31 @@ import logo from "../assets/vehiclemanagementlogo.svg";
 import { ShoppingCart } from "lucide-react";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const { cartCount } = useContext(CartContext);
+  const { userData } = useContext(AppContext);
   return (
     <nav className="flex justify-between px-6 py-3 mx-auto border-b-2 shadow-sm">
       <Link to={"/"} className="flex items-center">
         <img src={logo} alt="Logo" width={200} height={200} />
       </Link>
       <div className="flex items-center space-x-4">
-        <Link
-          className="relative p-3 transition-all duration-300 ease-in-out bg-white border rounded-full cursor-pointer dark:bg-gray-800 dark:border-white/20 hover:dark:border-white border-black/40 hover:border-black"
-          to={"/cart"}
-        >
-          <ShoppingCart className="w-4 h-4 " />
-          {cartCount > 0 && (
-            <span className="absolute flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full -top-1 -right-1">
-              {cartCount > 99 ? "99+" : cartCount}
-            </span>
-          )}
-        </Link>
+        {userData?.role === "USER" && (
+          <Link
+            className="relative p-3 transition-all duration-300 ease-in-out bg-white border rounded-full cursor-pointer dark:bg-gray-800 dark:border-white/20 hover:dark:border-white border-black/40 hover:border-black"
+            to={"/cart"}
+          >
+            <ShoppingCart className="w-4 h-4 " />
+            {cartCount > 0 && (
+              <span className="absolute flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full -top-1 -right-1">
+                {cartCount > 99 ? "99+" : cartCount}
+              </span>
+            )}
+          </Link>
+        )}
+
         <ProfileHudTop />
       </div>
     </nav>
